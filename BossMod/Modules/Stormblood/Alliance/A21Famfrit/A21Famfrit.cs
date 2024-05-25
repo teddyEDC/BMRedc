1628da2d840066fad33b3ccfca08e3af34ce4959
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-05-25 18:29:58 -0500

    Arena corrections

--- BossMod/Modules/Stormblood/Alliance/A21Famfrit/A21Famfrit.cs
+++ BossMod/Modules/Stormblood/Alliance/A21Famfrit/A21Famfrit.cs
@@ -1,4 +1,4 @@
 ﻿namespace BossMod.Stormblood.Alliance.A21Famfrit;
 
 [ModuleInfo(BossModuleInfo.Maturity.WIP, Contributors = "CombatReborn Team", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 550, NameID = 7245)]
-public class A21Famfrit(WorldState ws, Actor primary) : BossModule(ws, primary, new(-200, 66), new ArenaBoundsCircle(35));
+public class A21Famfrit(WorldState ws, Actor primary) : BossModule(ws, primary, new(-200, 66.5f), new ArenaBoundsCircle(30));
