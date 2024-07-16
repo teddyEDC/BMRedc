9ee2e64f6b4a49973c04b3c24985695f076cfff7
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2024-07-16 09:21:42 -0500

    through an announcer's table

---
+++ BossMod/Modules/Dawntrail/Raid/M2NHoneyB/M2NHoneyBStates.cs
@@ -0,0 +1,28 @@
+﻿namespace BossMod.Dawntrail.Raid.M2NHoneyB;
+
+class M2NHoneyBStates : StateMachineBuilder
+{
+    public M2NHoneyBStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<CallMeHoney>()
+            .ActivateOnEnter<TemptingTwist3>()
+            .ActivateOnEnter<TemptingTwist4>()
+            .ActivateOnEnter<HoneyBeeline3>()
+            .ActivateOnEnter<HoneyBeeline4>()
+            //.ActivateOnEnter<HoneyedBreeze>()
+            .ActivateOnEnter<HoneyBLive1>()
+            .ActivateOnEnter<Heartsore>()
+            .ActivateOnEnter<Fracture>()
+            .ActivateOnEnter<Loveseeker2>()
+            .ActivateOnEnter<BlowKiss>()
+            .ActivateOnEnter<HoneyBFinale>()
+            .ActivateOnEnter<DropOfVenom2>()
+            .ActivateOnEnter<BlindingLove3>()
+            .ActivateOnEnter<BlindingLove4>()
+            .ActivateOnEnter<HeartStruck1>()
+            .ActivateOnEnter<HeartStruck2>()
+            .ActivateOnEnter<HeartStruck3>();
+    }
+}
+
---
+++ BossMod/Modules/Dawntrail/Raid/M4NWickedThunder/M4NWickedThunderStates.cs
@@ -0,0 +1,21 @@
+﻿namespace BossMod.Dawntrail.Raid.M4NWickedThunder;
+
+class M4NWickedThunderStates : StateMachineBuilder
+{
+    public M4NWickedThunderStates(BossModule module) : base(module)
+    {
+        TrivialPhase()
+            .ActivateOnEnter<WrathOfZeus>()
+            .ActivateOnEnter<SidewiseSpark1>()
+            .ActivateOnEnter<SidewiseSpark2>()
+            .ActivateOnEnter<SidewiseSpark3>()
+            .ActivateOnEnter<SidewiseSpark4>()
+            .ActivateOnEnter<SidewiseSpark5>()
+            .ActivateOnEnter<SidewiseSpark6>()
+            .ActivateOnEnter<StampedingThunder3>()
+            .ActivateOnEnter<Burst>()
+            .ActivateOnEnter<BewitchingFlight3>()
+            .ActivateOnEnter<Thunderslam>()
+            .ActivateOnEnter<UnknownWeaponskill7>();
+    }
+}
