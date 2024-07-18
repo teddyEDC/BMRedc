8129d52b79884ee08d14d503a9114667eda26668
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2024-07-18 08:16:59 +0200

    copied from main branch for convenience

--- BossMod/Modules/Shadowbringers/Alliance/A31KnaveofHearts/A31KnaveofHeartsStates.cs
+++ BossMod/Modules/Shadowbringers/Alliance/A31KnaveofHearts/A31KnaveofHeartsStates.cs
@@ -4,6 +4,20 @@ class A31KnaveofHeartsStates : StateMachineBuilder
 {
     public A31KnaveofHeartsStates(BossModule module) : base(module)
     {
-        TrivialPhase();
+        TrivialPhase()
+            .ActivateOnEnter<Roar>()
+            .ActivateOnEnter<ColossalImpactLeft>()
+            .ActivateOnEnter<ColossalImpactRight>()
+            .ActivateOnEnter<ColossalImpactMiddle>()
+            .ActivateOnEnter<ColossalImpact6>()
+            .ActivateOnEnter<ColossalImpact7>()
+            .ActivateOnEnter<ColossalImpact8>()
+            .ActivateOnEnter<MagicArtilleryBeta2>()
+            .ActivateOnEnter<MagicArtilleryAlpha2>()
+            .ActivateOnEnter<Energy>()
+            .ActivateOnEnter<LightLeap2>()
+            .ActivateOnEnter<BoxSpawn>()
+            .ActivateOnEnter<MagicBarrage>()
+            .ActivateOnEnter<Lunge>();
     }
 }
---
+++ BossMod/Modules/Shadowbringers/Alliance/A34RedGirlP1/A34RedGirlP1States.cs
@@ -0,0 +1,9 @@
+﻿namespace BossMod.Shadowbringers.Alliance.A34RedGirlP1;
+
+class A34RedGirlP1States : StateMachineBuilder
+{
+    public A34RedGirlP1States(BossModule module) : base(module)
+    {
+        TrivialPhase();
+    }
+}
