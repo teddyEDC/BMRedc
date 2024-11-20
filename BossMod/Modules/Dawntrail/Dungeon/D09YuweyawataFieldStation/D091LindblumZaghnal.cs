a15a5904681e38b1346002b3e45b1d9475c5c637
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2024-11-20 20:11:15 +0100

    Yuweyawata Field Station trash modules

--- BossMod/Modules/Dawntrail/Dungeon/D09YuweyawataFieldStation/D091LindblumZaghnal.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D09YuweyawataFieldStation/D091LindblumZaghnal.cs
@@ -102,7 +102,7 @@ class D091LindblumZaghnalStates : StateMachineBuilder
     }
 }
 
-[ModuleInfo(BossModuleInfo.Maturity.Verified, Contributors = "The Combat Reborn Team (Malediktus)", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1008, NameID = 13623)]
+[ModuleInfo(BossModuleInfo.Maturity.Verified, Contributors = "The Combat Reborn Team (Malediktus)", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1008, NameID = 13623, SortOrder = 3)]
 public class D091LindblumZaghnal(WorldState ws, Actor primary) : BossModule(ws, primary, arena.Center, arena)
 {
     private static readonly ArenaBoundsComplex arena = new([new Polygon(new(73, 277), 19.5f * CosPI.Pi40th, 40)], [new Rectangle(new(72, 297), 20, 1.1f),
