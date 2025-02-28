3932c23190fdc69d9d426ca9c670954c3b18b0ce
CarnifexOptimus <156172553+CarnifexOptimus@users.noreply.github.com>
2025-02-28 16:12:36 +0100

    final fix for rectSE

--- BossMod/BossModule/Shapes.cs
+++ BossMod/BossModule/Shapes.cs
@@ -151,7 +151,7 @@ public sealed record class RectangleSE(WPos Start, WPos End, float HalfWidth) : 
     Center: new((Start.X + End.X) * Half, (Start.Z + End.Z) * Half),
     HalfWidth: HalfWidth,
     HalfHeight: (End - Start).Length() * Half,
-    Rotation: new Angle(MathF.Atan2(End.Z - Start.Z, End.X - Start.X))
+    Rotation: new Angle(MathF.Atan2(End.X - Start.X, End.Z - Start.Z))
 );
 
 public sealed record class Square(WPos Center, float HalfSize, Angle Rotation = default) : Rectangle(Center, HalfSize, HalfSize, Rotation);
