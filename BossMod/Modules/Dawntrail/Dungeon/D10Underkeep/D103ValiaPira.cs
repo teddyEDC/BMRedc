db4796400e41822ef029d76456bc0a02c5a87f45
Chuggalo <m1903chuggalo@yahoo.com>
2025-04-08 04:35:15 -0400

    Update D103ValiaPira.cs

--- BossMod/Modules/Dawntrail/Dungeon/D10Underkeep/D103ValiaPira.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D10Underkeep/D103ValiaPira.cs
@@ -1,4 +1,3 @@
-using Dalamud.Game.ClientState.Conditions;
 namespace BossMod.Dawntrail.Dungeon.D10Underkeep.D103ValiaPira;
 
 public enum OID : uint
@@ -181,7 +180,4 @@ class D103ValiaPiraStates : StateMachineBuilder
 }
 
 [ModuleInfo(BossModuleInfo.Maturity.Verified, Contributors = "The Combat Reborn Team (Malediktus)", GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1027, NameID = 13749)]
-public class D103ValiaPira(WorldState ws, Actor primary) : BossModule(ws, primary, new(default, -331f), new ArenaBoundsSquare(17.5f))
-{
-    public override bool CheckReset() => !Service.Condition[ConditionFlag.BoundByDuty];
-}
+public class D103ValiaPira(WorldState ws, Actor primary) : BossModule(ws, primary, new(default, -331f), new ArenaBoundsSquare(17.5f));
